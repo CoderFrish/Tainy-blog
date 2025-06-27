4649fc50278b4aac79f256577dd1571ac4faa316
Frish2021 <1573880184@qq.com>
2025-06-27 18:54:01 +0800

    添加VITE_APP_TITLE变量

--- tainy-v3/vite.config.ts
+++ tainy-v3/vite.config.ts
@@ -1,12 +1,20 @@
 import path from 'path'
 import vuePlugin from '@vitejs/plugin-vue'
 import { defineConfig, loadEnv } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd())
 
     return {
-        plugins: [vuePlugin()],
+        plugins: [vuePlugin(), createHtmlPlugin({
+            minify: true,
+            inject: {
+                data: {
+                    title: env.VITE_APP_TITLE
+                }
+            }
+        })],
         resolve: {
             alias: {
                 "@": path.resolve(__dirname, 'src')
